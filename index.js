ea5d93e9579f42e19c2e4cc51325711b86a067d4
dashboy <le.rue@protonmail.com>
2020-03-13 16:37:28 +0000

    all database, rest api, splited params

---
+++ index.js
@@ -0,0 +1,67 @@
+const express=require('express');
+const mysql=require('mysql');
+
+const app=express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "LIGHTNING_TEAM"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to MariaDB!");
+});
+
+app.set('port',25612);
+app.listen(app.get('port'), function(){
+	console.log('Listening on : ', app.get('port'));
+				      });
+
+//receive post request from nagios api (PHP)
+app.post('/nagios/:nagios', async function(req,res){
+  console.log(req.params.nagios);
+  res.send(req.params);
+  let pst= req.params;
+  i=pst.split(';');
+  let name=i[0];
+  let data=i[1];
+  Format {"nagios":"movistar;123456"} http://165.227.204.184:25612/nagios/movistar;123456
+  await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'nagios', ",name,", ", data, ", NOW());");
+});
+
+//receive post request from zabbix api (PYTHON)
+app.post('/zabbix/:name/:zabbix', async function(req,res){
+  console.log(req.params.zabbix);
+  res.send(req.params.zabbix);
+  let pst= req.params;
+  i=pst.split(';');
+  let name=i[0];
+  let data=i[1];
+  await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'zabbix', ",name,", ", data, ", NOW());");
+});
+
+//receive post request from splunk api (PYTHON)
+app.post('/splunk/:name/:splunk', async function(req,res){
+  console.log(req.params.splunk);
+  res.send(req.params);
+  let pst= req.params;
+  i=pst.split(';');
+  let name=i[0];
+  let data=i[1];
+  await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'splunk', ",name,", ", data, ", NOW());");
+});
+
+//receive post request from zendesk api (PYTHON)
+app.post('/zendesk/:name/:zendesk', async function(req,res){
+  console.log(req.params.zendesk);
+  res.send(req.params);
+  let pst= req.params;
+  i=pst.split(';');
+  let name=i[0];
+  let data=i[1];
+  await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'zendesk', ",name,", ", data, ", NOW());");
+});
+
