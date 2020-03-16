655de26af7a6f2d7ff42c996118abf733110c13f
dashboy <le.rue@protonmail.com>
2020-03-16 16:59:11 +0000

    database mysql 1 row inserted, ok

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const express=require('express');
-//const mysql=require('mysql');
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
-     host: 'localhost',
-     user:'root',
-     password: '',
-     database: "LIGHTNING_TEAM",
-     connectionLimit: 5
-});
+const mysql=require('mysql');
+const crypto = require('crypto');
+//const mariadb = require('mariadb');
+// const pool = mariadb.createPool({
+//     host: 'localhost',
+//     user:'root',
+//     password: '',
+//    database: "LIGHTNING_TEAM",
+//     connectionLimit: 5
+// });
 
 const app=express();
 
-//var con = mysql.createConnection({
-//  host: "localhost",
-//  user: "root",
-//  password: "",
-//  port : "3306",
-//  database: "LIGHTNING_TEAM"
-//});
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  port : "3306",
+  database: "LIGHTNING_TEAM"
+});
 
-//con.connect(function(err) {
-//  if (err) throw err;
-//  console.log("Connected to MariaDB!");
-//});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to MariaDB!");
+});
 
 app.set('port',25612);
 app.listen(app.get('port'), function(){
@@ -32,18 +33,27 @@ app.listen(app.get('port'), function(){
 //  Format => "name;[{data}]" http://165.227.204.184:25612/nagios/movistar;123456
 
 //receive post request from nagios api (PHP)
-app.post('/nagios/:nagios', async function(req,res){
+app.post('/nagios/:nagios', function(req,res){
   console.log(req.params.nagios);
   res.send(req.params);
   let pst= req.params;
   i=pst.nagios.split(';');
   let name=i[0];
   let data=JSON.stringify(i[1]);
+  //var key = crypto.createCipher('aes-128-cbc', 'mypassword');
+  //var str = key.update(data, 'utf8', 'hex')
+  //str += key.final('hex');
+  //console.log(str);
+  //data=str;
   console.log("Name => ", name);
   console.log("Data => ", data);
-  //let con = await pool.getConnection();
-  //const mdb = await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'nagios', ",name,", ", data, ", NOW());",[1, "mariadb"]);
- // console.log(mdb);
+  let sql="INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'nagios', '" + name + "', '" + data + "', NOW());";
+  console.log("SQL => ", sql);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+  //con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'nagios', ",name,", ", data, ", NOW());");
 });
 
 //receive post request from zabbix api (PYTHON)
@@ -56,6 +66,12 @@ app.post('/zabbix/:zabbix', async function(req,res){
   let data=JSON.stringify(i[1]);
   console.log("Name => ", name);
   console.log("Data => ", data);
+  let sql="INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'nagios', '" + name + "', '" + data + "', NOW());";
+  console.log("SQL => ", sql);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
   //await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'zabbix', ",name,", ", data, ", NOW());");
 });
 
@@ -69,6 +85,12 @@ app.post('/splunk/:splunk', async function(req,res){
   let data=JSON.stringify(i[1]);
   console.log("Name => ", name);
   console.log("Data => ", data);
+  let sql="INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'nagios', '" + name + "', '" + data + "', NOW());";
+  console.log("SQL => ", sql);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
   //await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'splunk', ",name,", ", data, ", NOW());");
 });
 
@@ -82,6 +104,12 @@ app.post('/zendesk/:zendesk', async function(req,res){
   let data=JSON.stringify(i[1]);
   console.log("Name => ", name);
   console.log("Data => ", data);
+  let sql="INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'nagios', '" + name + "', '" + data + "', NOW());";
+  console.log("SQL => ", sql);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
   //await con.query("INSERT INTO TASKS(SEQ, MONITOR, CLIENT_NAME, DATA_STREAM, TIME_EVENT) VALUES(0, 'zendesk', ",name,", ", data, ", NOW());");
 });
 
